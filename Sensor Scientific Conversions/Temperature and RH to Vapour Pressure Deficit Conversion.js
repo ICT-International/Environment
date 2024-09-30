38937f280182c20865ab5825ba2f8bbf43c1175d
Toby-ICT-International <toby@ictinternational.com.au>
2024-09-30 16:05:41 +1000

    Add files via upload

---
+++ Sensor Scientific Conversions/Temperature and RH to Vapour Pressure Deficit Conversion.js
@@ -0,0 +1,18 @@
+/*ICT International
+Example Javascript for converting an Air Temperature and Humidity Sensor to Vapour Pressure deficit
+*/
+			var air_temperature = "Air Temperature Sensor" //Unit: °C, Convert to var to feed both array push and VPD calc.
+			var relative_humidity = "Relative Humidity Sensor" //Unit 0-100%, Convert to var to feed both array push and VPD calc.
+			var svcal = ((17.27 * air_temperature)/(237.3 + air_temperature)); //1st calc step SVP.
+			var svp = 0.6108 * Math.exp(svcal); //2nd calc step, Saturated Vapour Pressure in Pascals.
+			var vpdcalc = ((100 - relative_humidity)/100) * svp; //AVP & Calculated VPD.
+			arr.push(["vapour-pressure-deficit", 0, vpdcalc, src, "kPa"]); //Print to Array (Example)
+/*
+References or Updates:
+Validated 30/Sep/2024
+Based on the FAO Calculations	
+1	https://www.fao.org/4/y7223e/y7223e0i.htm
+	saturation vapour pressure (es)
+2	https://www.fao.org/4/X0490E/x0490e06.htm
+	ETO
+*/
